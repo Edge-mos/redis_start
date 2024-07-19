9687a055f9dac4c6febb32a4c00f09e13ae96186
Vladimir Yamnikov <Androedge@gmail.com>
2024-07-19 21:48:33 +0300

    sprint project initiated

---
+++ src/main/java/com/androedge/spredis/SpringRedisStartApplication.java
@@ -0,0 +1,13 @@
+package com.androedge.spredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisStartApplication.class, args);
+	}
+
+}
